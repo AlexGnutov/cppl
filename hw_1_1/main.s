7972216fbd82e230ee341b0d39a316d7f2fd4a89
AlexGnutov <->
2023-12-09 08:15:03 +0300

    hw_1_1

---
+++ hw_1_1/main.s
@@ -0,0 +1,213 @@
+	.file	"main.cpp"
+	.text
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.section	.text._ZN5PointC2Edd,"axG",@progbits,_ZN5PointC5Edd,comdat
+	.align 2
+	.weak	_ZN5PointC2Edd
+	.type	_ZN5PointC2Edd, @function
+_ZN5PointC2Edd:
+.LFB1732:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movsd	%xmm0, -16(%rbp)
+	movsd	%xmm1, -24(%rbp)
+	movq	-8(%rbp), %rax
+	movsd	-16(%rbp), %xmm0
+	movsd	%xmm0, (%rax)
+	movq	-8(%rbp), %rax
+	movsd	-24(%rbp), %xmm0
+	movsd	%xmm0, 8(%rax)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1732:
+	.size	_ZN5PointC2Edd, .-_ZN5PointC2Edd
+	.weak	_ZN5PointC1Edd
+	.set	_ZN5PointC1Edd,_ZN5PointC2Edd
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1734:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$48, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$0, -36(%rbp)
+	jmp	.L3
+.L4:
+	movl	-36(%rbp), %eax
+	addl	%eax, %eax
+	pxor	%xmm0, %xmm0
+	cvtsi2sdl	%eax, %xmm0
+	pxor	%xmm2, %xmm2
+	cvtsi2sdl	-36(%rbp), %xmm2
+	movq	%xmm2, %rdx
+	leaq	-32(%rbp), %rax
+	movapd	%xmm0, %xmm1
+	movq	%rdx, %xmm0
+	movq	%rax, %rdi
+	call	_ZN5PointC1Edd
+	leaq	-32(%rbp), %rax
+	movq	%rax, %rdi
+	call	_Z11print_pointRK5Point
+	addl	$1, -36(%rbp)
+.L3:
+	cmpl	$4, -36(%rbp)
+	jle	.L4
+	movl	$0, %eax
+	movq	-8(%rbp), %rdx
+	subq	%fs:40, %rdx
+	je	.L6
+	call	__stack_chk_fail@PLT
+.L6:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1734:
+	.size	main, .-main
+	.section	.rodata
+.LC0:
+	.string	"x:"
+.LC1:
+	.string	", y: "
+	.text
+	.globl	_Z11print_pointRK5Point
+	.type	_Z11print_pointRK5Point, @function
+_Z11print_pointRK5Point:
+.LFB1735:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%rdi, -8(%rbp)
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rsi
+	leaq	_ZSt4cout(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-8(%rbp), %rax
+	movq	(%rax), %rax
+	movq	%rax, %xmm0
+	movq	%rdx, %rdi
+	call	_ZNSolsEd@PLT
+	movq	%rax, %rdx
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-8(%rbp), %rax
+	movq	8(%rax), %rax
+	movq	%rax, %xmm0
+	movq	%rdx, %rdi
+	call	_ZNSolsEd@PLT
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rdx
+	movq	%rdx, %rsi
+	movq	%rax, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1735:
+	.size	_Z11print_pointRK5Point, .-_Z11print_pointRK5Point
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2239:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L10
+	cmpl	$65535, -8(%rbp)
+	jne	.L10
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rax
+	movq	%rax, %rdx
+	leaq	_ZStL8__ioinit(%rip), %rax
+	movq	%rax, %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L10:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2239:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I_main, @function
+_GLOBAL__sub_I_main:
+.LFB2240:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2240:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
